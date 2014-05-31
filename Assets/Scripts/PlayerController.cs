0f99e90b570a5eec1c832113b8a6b7465573fc3c
Shanan Dalton <shanan_dalton@yahoo.com>
2014-05-31 18:50:18 -0400

    Basic player movement and Followers
    Added scripts for the player and the people who follow you

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,20 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed = 5f;
-
 	public int numFollowers = 0;
+	public float followerOffset = .25f;
 
-	public float h;
+	private ArrayList followerList;
+	private ArrayList followAnim;
+	private float h;
 	private Animator anim;
 	private bool facingRight = true;
 	
 	void Start () {
 
 		anim = GetComponent<Animator>();
-	
+		followerList = new ArrayList();
+		followAnim = new ArrayList();
 	}
 
 	void Update () {
@@ -39,6 +42,11 @@ public class PlayerController : MonoBehaviour {
 		anim.SetFloat("Speed", Mathf.Abs(h));
 		rigidbody2D.velocity = new Vector2(h * moveSpeed, rigidbody2D.velocity.y);
 
+		//Update all the follower animations
+		foreach(Animator anima in followAnim){
+			anima.SetFloat("Speed", Mathf.Abs(h));
+		}
+
 
 
 	}
@@ -46,18 +54,76 @@ public class PlayerController : MonoBehaviour {
 	private void FlipSprite(){
 
 		facingRight = !facingRight;
-		
+
 		// Multiply the player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
+
+		if(followerList.Count != 0){
+			foreach(GameObject gameObj in followerList){
+				gameObj.transform.parent = null;
+				gameObj.transform.localScale = theScale;
+			}
+		}
+
 		transform.localScale = theScale;
+
+		if(followerList.Count != 0){
+
+			foreach(GameObject gameObj in followerList){
+				gameObj.transform.parent = gameObject.transform;
+			}
+
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
 
 		if(col.gameObject.tag == "Protester"){
+			col.gameObject.layer = 11;
+
+			followerList.Add(col.gameObject);
+			followAnim.Add(col.gameObject.GetComponent<Animator>());
+
+			//Destroy(col.gameObject.rigidbody2D);
+			//Destroy(col.collider);
+			//Destroy(col.gameObject.GetComponent<Protester>());
+
+
+			col.transform.parent = this.gameObject.transform;
+
+			if(numFollowers%2 == 0){
+				col.transform.position = new Vector3(transform.position.x + followerOffset +(followerOffset*numFollowers) , transform.position.y, 0);
+			}else{
+				col.transform.position = new Vector3(transform.position.x - followerOffset - (followerOffset*numFollowers) , transform.position.y, 0);
+			}
+
 			numFollowers++;
+
+		
+
 		}
 
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
---
+++ Assets/Scripts/Protester.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Protester : MonoBehaviour {
+	
+	public float jumpTimer = 2f;
+	public float jumpHeight = 1f;
+
+	public float jumpCountdown = 0f;
+	private bool canJump = false;
+	private bool canDrop = false;
+
+
+	private float jumpStart;
+	private float jumpDest;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+
+
+		if(jumpCountdown >= jumpTimer){
+			canJump = true;
+			jumpStart = transform.position.y;
+			jumpDest = transform.position.y + jumpHeight;
+			jumpCountdown = 0f;
+		}else if(!canJump && !canDrop){
+			jumpCountdown += Time.deltaTime;
+		}
+
+
+	
+	}
+
+	void FixedUpdate(){
+
+		if(canJump){
+			rigidbody2D.MovePosition(new Vector2(transform.position.x, Mathf.MoveTowards(transform.position.y, jumpDest, .2f)));
+
+			if(transform.position.y == jumpDest){
+				canDrop = true;
+				canJump = false;
+			}
+
+		}
+
+		if(canDrop){
+			rigidbody2D.MovePosition(new Vector2(transform.position.x, Mathf.MoveTowards(transform.position.y, jumpStart, .2f)));
+
+			if(transform.position.y == jumpStart){
+				canDrop = false;
+			}
+		}
+	}
+}
