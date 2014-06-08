cc2460d926c8fcec976f61110e3951851150237b
Shanan Dalton <shanan_dalton@yahoo.com>
2014-06-08 02:57:44 -0400

    Added image and worked on scripts
    Added the ladder image.
    
    Rewrote part of the player movement to make it more suitable for future
    use.
    
    Currently working on getting a pyramid of followers working(not
    finished yet), will work on refining their movement at a later time.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,101 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(Animator))]
 
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed = 5f;
+	public float jumpForce = 3f;
 	public int numFollowers = 0;
 	public float followerOffset = .25f;
-
-	private ArrayList followerList;
-	private ArrayList followAnim;
+	
+	private List<GameObject> followerList;
+	private List<Protester> followerScripts;
+	private List<Animator> followAnim;
 	private float h;
+	private float v;
+	private float tempVert;
 	private Animator anim;
 	private bool facingRight = true;
+
+	private Transform groundCheck;
+	private Transform ceilingCheck;
+	private float groundedRadius = .2f;
+	private float ceilingRadius = .1f;
+	public bool grounded = false;
+	public bool specialGrounded = false;
+	public LayerMask whatIsGround;
+	public LayerMask whatIsSpecialGround;
+	private bool prevGroundedState;
+
+	private bool jump = false;
+
 	
 	void Start () {
 
+		// Setting up references.
+		groundCheck = transform.Find("GroundCheck");
+		ceilingCheck = transform.Find("CeilingCheck");
 		anim = GetComponent<Animator>();
-		followerList = new ArrayList();
-		followAnim = new ArrayList();
+
+		//Setting up Lists
+		followerList = new List<GameObject>();
+		followerScripts = new List<Protester>();
+		followAnim = new List<Animator>();
+
 	}
 
 	void Update () {
 
+		if(Input.GetKeyDown(KeyCode.Space) && (grounded || specialGrounded) ){
+			jump = true;
+		}
 
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
+
+		//Special grounded includes standing on a follower or other unique things
+		specialGrounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsSpecialGround);
+
+		//Prevent looping through each list every frame
+		if(prevGroundedState != grounded){
+
+			if(grounded){
+				foreach(Protester prot in followerScripts){
+					prot.setLayer(11);
+					prot.playerGrounded = true;
+				}
+			}else{
+				foreach(Protester prot in followerScripts){
+					prot.setLayer(9);
+					prot.playerGrounded = false;
+				}
+			}
+
+		}
+		prevGroundedState = grounded;
+
+		//TODO organize people that are used into their own group and remove them from the arraylist
+		//Remember to add them back in to the followerScript list later
+		//dont forget to update the current num of followers to make sure it doesnt grab the wrong person(or have it start grabbing people from 0)
+		//Starts a pyramid of people
+		if(Input.GetKeyDown(KeyCode.UpArrow) && specialGrounded && (numFollowers > 3)){
+
+			followerScripts[numFollowers-1].hopToLocation(this.transform.position);
+
+		}
 
 	}
 
 	void FixedUpdate(){
 
 		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
 
 		//Sets the direction that the player needs to face based on the movement
 		if(h > 0 && !facingRight){
@@ -39,15 +104,21 @@ public class PlayerController : MonoBehaviour {
 			FlipSprite();
 		}
 
+		//Set Play animations
 		anim.SetFloat("Speed", Mathf.Abs(h));
 		rigidbody2D.velocity = new Vector2(h * moveSpeed, rigidbody2D.velocity.y);
 
-		//Update all the follower animations
-		foreach(Animator anima in followAnim){
-			anima.SetFloat("Speed", Mathf.Abs(h));
+		//Update all the followers
+		updateFollowers(h, grounded);
+
+
+		// If the player should jump...
+		if ((grounded || specialGrounded) && jump) {
+			// Add a vertical force to the player.
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+			specialGrounded = false;
 		}
-
-
 
 	}
 
@@ -61,13 +132,14 @@ public class PlayerController : MonoBehaviour {
 
 		if(followerList.Count != 0){
 			foreach(GameObject gameObj in followerList){
-				gameObj.transform.parent = null;
+				//gameObj.transform.parent = null;
 				gameObj.transform.localScale = theScale;
 			}
 		}
 
 		transform.localScale = theScale;
 
+		/*
 		if(followerList.Count != 0){
 
 			foreach(GameObject gameObj in followerList){
@@ -75,21 +147,45 @@ public class PlayerController : MonoBehaviour {
 			}
 
 		}
+		*/
+	}
+
+	void updateFollowers(float playerMovement, bool isPlayerGrounded){
+
+		foreach(Animator anima in followAnim){
+			anima.SetFloat("Speed", Mathf.Abs(playerMovement));
+		}
+
+		/*
+		foreach(Protester prot in followerScripts){
+			prot.playerGrounded = isPlayerGrounded;
+		}
+		*/
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
 
 		if(col.gameObject.tag == "Protester"){
-			col.gameObject.layer = 11;
+
+			//Prevents collision with the protester
+			//col.gameObject.layer = 11;
 
 			followerList.Add(col.gameObject);
+			followerScripts.Add(col.gameObject.GetComponent<Protester>());
 			followAnim.Add(col.gameObject.GetComponent<Animator>());
+
+			col.gameObject.GetComponent<Protester>().followTarget = this.transform;
+			col.gameObject.GetComponent<Protester>().canFollow = true;
+			col.gameObject.GetComponent<Protester>().playerGrounded = grounded;
+			col.gameObject.GetComponent<Protester>().setLayer(11);
+			col.gameObject.tag = "Follower";
 
 			//Destroy(col.gameObject.rigidbody2D);
 			//Destroy(col.collider);
 			//Destroy(col.gameObject.GetComponent<Protester>());
 
-
+			/*
+			Set protester as a child to follow the player directly
 			col.transform.parent = this.gameObject.transform;
 
 			if(numFollowers%2 == 0){
@@ -97,6 +193,8 @@ public class PlayerController : MonoBehaviour {
 			}else{
 				col.transform.position = new Vector3(transform.position.x - followerOffset - (followerOffset*numFollowers) , transform.position.y, 0);
 			}
+			*/
+
 
 			numFollowers++;
 
--- Assets/Scripts/Protester.cs
+++ Assets/Scripts/Protester.cs
@@ -2,25 +2,44 @@
 using System.Collections;
 
 public class Protester : MonoBehaviour {
-	
+
+	/*
 	public float jumpTimer = 2f;
 	public float jumpHeight = 1f;
-
 	public float jumpCountdown = 0f;
 	public bool canJump = false;
 	public bool canDrop = false;
+	*/
 
+	public Transform followTarget;
+	public float smoothTime = .3f;
+	public float xOffset = 1f;
+	public float yOffset = 1f;
+	public bool canFollow = false;
+	public bool canHop = false;
+	public bool playerGrounded = false;
+	public float speedBoost = 2f;
+	public float hopBoost = 10f;
+
+	private Transform thisTransform;
+	private Vector2 thisPosition;
+	//private Vector2 velocity;
+	private Vector2 hopPosition;
+	 
 
 	private float jumpStart;
 	private float jumpDest;
 
 	void Start () {
+
+		thisTransform = transform;
+		thisPosition = thisTransform.position;
 	
 	}
 
 	void Update () {
 
-
+		/*
 		if(jumpCountdown >= jumpTimer){
 			canJump = true;
 			jumpStart = transform.position.y;
@@ -29,8 +48,28 @@ public class Protester : MonoBehaviour {
 		}else if(!canJump && !canDrop){
 			jumpCountdown += Time.deltaTime;
 		}
+		*/
 
 
+		if(canFollow && playerGrounded){
+			thisPosition.x = Mathf.Lerp( thisTransform.position.x, followTarget.position.x + xOffset, Time.deltaTime * speedBoost);
+			thisPosition.y = Mathf.Lerp( thisTransform.position.y, followTarget.position.y + yOffset,  Time.deltaTime * speedBoost);
+			thisTransform.position = thisPosition;
+		}
+
+		if(canHop){
+
+			thisPosition.x = Mathf.Lerp( thisTransform.position.x, hopPosition.x, Time.deltaTime * hopBoost);
+			thisPosition.y = Mathf.Lerp( thisTransform.position.y, hopPosition.y,  Time.deltaTime * hopBoost);
+			thisTransform.position = thisPosition;
+
+			if((Vector2)transform.position == hopPosition){
+				canHop = false;
+				gameObject.layer = 9;
+				rigidbody2D.isKinematic = false;
+			}
+
+		}
 
 
 	
@@ -38,6 +77,7 @@ public class Protester : MonoBehaviour {
 
 	void FixedUpdate(){
 
+		/*
 		if(canJump){
 
 			
@@ -60,7 +100,48 @@ public class Protester : MonoBehaviour {
 				canDrop = false;
 			}
 		}
+		*/
 
 
+		//thisTransform.position.x = Mathf.Lerp( thisTransform.position.x, followTarget.position.x + xOffset, Time.deltaTime * smoothTime);
+		//thisTransform.position.y = Mathf.Lerp( thisTransform.position.y, followTarget.position.y + yOffset, Time.deltaTime * smoothTime);
+		
+
+
+		/*
+		if(playerGrounded == false){
+			
+			gameObject.layer = 9;
+			
+		}else{
+			
+			gameObject.layer = 11;
+			
+		}
+		*/
+
+
+
+
+
+
+
+
+	}
+
+	void LateUpdate(){
+
+	}
+
+	public void setLayer(int layerToMoveProtesterTo){
+		gameObject.layer = layerToMoveProtesterTo;
+	}
+
+	public void hopToLocation(Vector2 hopLoc){
+
+		hopPosition = hopLoc;
+		canHop = true; 
+		gameObject.layer = 11;
+		rigidbody2D.isKinematic = true;
 	}
 }
